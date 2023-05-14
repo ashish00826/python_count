6fb76aacb1cb9669ea20747e6d7865b542f312cc
ashish00826 <133553390+ashish00826@users.noreply.github.com>
2023-05-14 15:43:57 -0400

    Create terraform_count.py

---
+++ terraform_count.py
@@ -0,0 +1,13 @@
+	resource "null_resource" "python_file_writer" {
+  provisioner "local-exec" {
+    command = "python3 ${path.module}/write_numbers.py"
+  }
+}
+
+write_numbers.py
+
+file_path = "numbers.txt"
+
+with open(file_path, "w") as file:
+    for number in range(1, 101):
+        file.write(str(number) + "\n")
